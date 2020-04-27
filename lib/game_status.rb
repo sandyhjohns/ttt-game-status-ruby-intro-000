ea51b352eac9d3033aaba4821e4b3a75d4ae3581
Sandy Johnson <sandythsu@gmail.com>
2020-04-27 15:51:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,3 +13,18 @@ WIN_COMBINATIONS =
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    if 
+      board[win_combination[0]] == 'X' && board[win_combination[1]=='X' && board [win_combination[2]] == 'X'
+      return "X wins with #{win_combination}"
+    elsif
+      board[win_combination[0]] == 'O' && board[win_combination[1]=='O' && board [win_combination[2]] == 'O'
+      return "O wins with #{win_combination}"
+    else
+      return false
+    end
+end
+  
+    
