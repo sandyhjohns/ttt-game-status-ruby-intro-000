2443242d8c9486ce821bd540843627e162819033
Sandy Johnson <sandythsu@gmail.com>
2020-04-27 16:15:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,10 +22,9 @@ def won?(board)
     elsif
       board[win_combination[0]] == 'O' && board[win_combination[1]] == 'O' && board [win_combination[2]] == 'O'
       return "O wins with #{win_combination}"
-    else
-      return false
     end
   end
+  return false
 end
 
     
